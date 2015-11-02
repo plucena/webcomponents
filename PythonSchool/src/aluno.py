8ae518d97c5bb410b92f1244ef36fb06fe23872a
percival@gmail.com <plucena@gmail.com>
2015-11-02 17:10:39 -0200

    Versao 1.0

---
+++ PythonSchool/src/aluno.py
@@ -0,0 +1,9 @@
+class Aluno:
+    
+    def __init__(self, ra, nome, email, curso):
+        self.ra = ra
+        self.nome = nome
+        self.email = email
+        self.curso = curso
+        
+    
---
+++ PythonSchool/src/teste.py
@@ -0,0 +1,5 @@
+import aluno
+
+print('Teste')
+aluno2 = aluno.Aluno(1222, 'Josival', 'josival@gmail.com', 'Sistemas') 
+print(aluno2.nome)
